bf54d0a25f9f787995a57aeb3004aa66459fb062
Jacobokia <cobs399@gmail.com>
2025-03-13 13:38:17 +0300

    add the logic for opening and closing the modal

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnOpenModel = document.querySelectorAll('.show-modal');
+const btnCloseModel = document.querySelector('.close-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModel.length; i++) {
+  btnOpenModel[i].addEventListener('click', openModal);
+}
+
+btnCloseModel.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+});
